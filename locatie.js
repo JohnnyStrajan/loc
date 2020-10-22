d18817999e3d0a6cbb35505adf9a80c932f03c72
JohnnyStrajan <72561098+JohnnyStrajan@users.noreply.github.com>
2020-10-22 16:56:14 +0300

    Add files via upload

--- locatie.js
+++ locatie.js
@@ -3,11 +3,11 @@ function on_success(position){
 	
 	document.getElementById("latitude").innerHTML = "Latitutde = " + position.coords.latitude;
 	
-	document.getElementById("longitude").innerHTML = "Longitude = " + position.coords.latitude;
+	document.getElementById("longitude").innerHTML = "Longitude = " + position.coords.longitude;
 	
-	document.getElementById("acc").innerHTML = "Accuracy = " + position.coords.latitude;
+	document.getElementById("acc").innerHTML = "Accuracy = " + position.coords.acc;
 	
-	document.getElementById("altitude").innerHTML = "Altitude = " + position.coords.latitude;
+	document.getElementById("altitude").innerHTML = "Altitude = " + position.coords.altitude;
 	
 	var img_url = "https://maps.googleapis.com/api/staticmap?center="+latlon+"&zoom=14&size=400x300&key=AIzaSyCkATcslyG_g6he4LoTZgIb44k_0Z2OhDQ";
 	
